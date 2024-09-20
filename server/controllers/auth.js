742114a928bc202cb9458fb770e9482be53278c1
Youngha Park <youngha.kkoti@gmail.com>
2024-09-20 15:41:31 +0900

    add login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,7 @@
 import bcrypt from "brcrypt";
 import jwt from "jsonwebtoken";
 import user from "../models/User.js";
+import User from "../models/User.js";
 
 /* REGISTER USER */
 export const register = async (req, res) => {
@@ -33,6 +34,24 @@ export const register = async (req, res) => {
     });
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist." });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials." });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
